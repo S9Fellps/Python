5416d87d1eea7de5ce47b9963e908416ebefaea3
Felipe Hentz <felipe.s.hentz@gmail.com>
2024-10-30 17:55:55 -0400

    Add files via upload

---
+++ 30do10.py
@@ -0,0 +1,14 @@
+if 5 > 2:
+    print ("Cinco é maior que dois")
+    print ("Cinco é maior que dois")
+    
+x= 'hello world'
+print (x)
+
+# Este é um comentario
+
+""""
+este é um comentario multilinhas
+ele continua sendo um texto mas 
+o python o ignora, entao é uma ganbiarra
+"""
